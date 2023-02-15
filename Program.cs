095b88b98112c778595b9e8309e97b3fb023f542
izoo0 <isaiahwainaina34@mgmail.com>
2023-02-15 04:44:32 +0300

    my first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace App{
+     class App{
+          public static int a;
+          public static int b;
+
+          public static int c;
+
+          public static int options;
+          public static void Main(string[] args)
+          {
+               Console.WriteLine("Please select an option");
+               Console.WriteLine(
+                    "1. +\n"+
+                    "2. -\n"+
+                    "3. *\n"+
+                    "4. /"
+               );
+               options = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Please Enter the first numebr");
+               a = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Please Enter the Second Number");
+               b = Convert.ToInt32(Console.ReadLine());
+               
+               Add();
+               Minus();
+               Multiply();
+               Division();
+               
+          }
+
+          public static  void Add(){
+               if (options == 1)
+               {
+                    c=a+b;
+                    Console.WriteLine("The Answer is" +" "+c);
+               }
+          }
+          public static  void Minus(){
+               if (options == 1)
+               {
+                    c=a+b;
+                    Console.WriteLine("The Answer is" +" "+c);
+               }
+          }
+          public static  void Multiply(){
+               if (options == 1)
+               {
+                    c=a+b;
+                    Console.WriteLine("The Answer is" +" "+c);
+               }
+          }
+          public static  void Division(){
+               if (options == 1)
+               {
+                    c=a+b;
+                    Console.WriteLine("The Answer is" +" "+c);
+               }
+          }
+     }
+}
